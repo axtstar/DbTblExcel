898eda028c553434482674ddf617ba544e6bdf6e
axt_star <axt_star@hotmail.com>
2021-09-10 17:36:21 +0900

    add mysql version

--- build.bat
+++ build.bat
@@ -1,2 +1,10 @@
-copy template\DBTBL.xlsm bin\
+del bin\*.xlsm
+copy template\DBTBL_postgres.xlsm bin\DBTBL.xlsm
 cscript vbac.wsf combine
+
+mv bin\DBTBL.xlsm bin\DBTBL_postgres.xlsm
+
+copy template\DBTBL_mysql.xlsm bin\DBTBL.xlsm
+cscript vbac.wsf combine
+
+mv bin\DBTBL.xlsm bin\DBTBL_mysql.xlsm
